60f1cf2d17c1e5ebdc9af399797e7cafa7188243
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2022-10-09 22:57:58 +0700

    Add about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,9 @@
-import { NextPage } from 'next'
-import Head from 'next/head'
-import MainGrid from '../components/MainGrid'
+import { NextPage } from "next";
+import Head from "next/head";
+import MainGrid from "../components/MainGrid";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 const About: NextPage = () => {
   return (
@@ -9,10 +12,40 @@ const About: NextPage = () => {
         <title>About</title>
       </Head>
       <MainGrid>
-
+        <Grid container item direction="column" alignItems="center" spacing={2}>
+          <Grid item mb={3}>
+            <Typography textAlign="center" variant="body1" color="white">
+              Aplikasi ini dibuat menggunakan Next.js
+            </Typography>
+          </Grid>
+          <Grid item mb={3}>
+            <Typography textAlign="center" variant="body1" color="white">
+              Hubungi saya
+              <br />
+              GitHub:{" "}
+              <a
+                style={{ textDecoration: "none" }}
+                href="https://github.com/fikrisyahid"
+              >
+                Fikri Syahid
+              </a>
+              <br />
+              Discord: fs3120#9002
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Button
+              href="https://github.com/fikrisyahid/teks-bapak-converter#readme"
+              variant="contained"
+              color="secondary"
+            >
+              Go to GitHub page
+            </Button>
+          </Grid>
+        </Grid>
       </MainGrid>
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
