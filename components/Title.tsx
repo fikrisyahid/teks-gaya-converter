10f87744fe6126f7c6a44f837c0afb1647e58201
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2022-10-09 22:05:14 +0700

    Change text align to center for mobile device

--- components/Title.tsx
+++ components/Title.tsx
@@ -5,7 +5,7 @@ import Grid from "@mui/material/Grid";
 const Title = () => {
   return (
     <Grid container mb={3} justifyContent="center">
-      <Typography variant="h2" fontWeight={700} color="white">
+      <Typography textAlign="center" variant="h2" fontWeight={700} color="white">
         Teks Bapak Converter
       </Typography>
     </Grid>
