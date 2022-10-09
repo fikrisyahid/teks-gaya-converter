ad4d9e01df0eb776355a8350f9bb89579b74f962
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2022-10-09 23:01:42 +0700

    Add word data

--- utils/word-data.ts
+++ utils/word-data.ts
@@ -1,7 +1,8 @@
 const wordData = {
   "gak": ["gk", "Gk", "gK", "gg", "Gg", "gG"],
   "tapi": ["tp", "tP", "Tp"],
-  "saya": ["sy", "Sy", "sY"]
+  "saya": ["sy", "Sy", "sY"],
+  "kan": ["kn", "Kn", "kN"]
 };
 
 export default wordData;
