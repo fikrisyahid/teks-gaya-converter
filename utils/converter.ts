674a35b5d54f2506b9145086874539574107582e
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2022-10-09 21:25:45 +0700

    Update converter

--- utils/converter.ts
+++ utils/converter.ts
@@ -1,5 +1,18 @@
+import wordData from "./word-data";
+import wordFetcher from "./word-fetcher";
+
 const converter = (input: String) => {
   let response = '';
+  const words = wordFetcher(input);
+  words.forEach((word) => {
+    if (wordData[word]) {
+      const index = Math.floor(Math.random() * wordData[word].length);
+      response += `${wordData[word][index]} `;
+    }
+    if (!wordData[word]) {
+      response += `${word} `;
+    }
+  });
   return response;
 }
  
