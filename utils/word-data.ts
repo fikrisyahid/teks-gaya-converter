5ad10e8022b85d6ac6667bb8843e97fdc525b763
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2022-10-09 23:05:08 +0700

    Add word data

--- utils/word-data.ts
+++ utils/word-data.ts
@@ -1,8 +1,12 @@
 const wordData = {
-  "gak": ["gk", "Gk", "gK", "gg", "Gg", "gG"],
-  "tapi": ["tp", "tP", "Tp"],
-  "saya": ["sy", "Sy", "sY"],
-  "kan": ["kn", "Kn", "kN"]
+  gak: ["gk", "Gk", "gK", "gg", "Gg", "gG"],
+  tapi: ["tp", "tP", "Tp"],
+  saya: ["sy", "Sy", "sY"],
+  kamu: ["km", "kM", "Km"],
+  kmu: ["km", "kM", "Km"],
+  gimana: ["gmn", "Gmn", "gMn", "gmN"],
+  gmn: ["gmn", "Gmn", "gMn", "gmN"],
+  maksud: ["mksud", "Mksud", "mKsud", "mkSud", "mksUd", "mksuD"]
 };
 
 export default wordData;
