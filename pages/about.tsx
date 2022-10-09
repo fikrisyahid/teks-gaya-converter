d4c09d3e194019fda9d3358b8a1b61b86f7a502b
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2022-10-09 22:22:24 +0700

    Change title for each page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,17 @@
 import { NextPage } from 'next'
+import Head from 'next/head'
 import MainGrid from '../components/MainGrid'
 
 const About: NextPage = () => {
   return (
-    <MainGrid>
-      
-    </MainGrid>
+    <>
+      <Head>
+        <title>About</title>
+      </Head>
+      <MainGrid>
+
+      </MainGrid>
+    </>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import MainGrid from "../components/MainGrid";
 import converter from "../utils/converter";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   const [input, setInput] = useState("");
@@ -17,72 +18,91 @@ const Home: NextPage = () => {
   };
 
   return (
-    <Grid container item>
-      <MainGrid>
-        <Grid container item mb={3}>
-          <Grid container item direction="column" xs={6} spacing={2} paddingRight={1}>
-            <Grid container item justifyContent="center">
-              <Grid item>
-                <Typography variant="h4" fontWeight={700} color="white">
-                  Input
-                </Typography>
-              </Grid>
-            </Grid>
-            <Grid item >
-              <TextField
-                multiline
-                fullWidth
-                minRows={5}
-                value={input}
-                color="primary"
-                onChange={(e) => setInput(e.target.value)}
-                inputProps={{
-                  style: { color: "white" },
-                  spellCheck: false,
-                }}
-                sx={{ backgroundColor: "#29363f", borderRadius: 3 }}
-              />
-            </Grid>
-          </Grid>
-          <Grid container item direction="column" xs={6} spacing={2} paddingLeft={1}>
-            <Grid container item justifyContent="center">
-              <Grid item>
-                <Typography variant="h4" fontWeight={700} color="white">
-                  Output
-                </Typography>
-              </Grid>
-            </Grid>
-            <Grid item>
-              <TextField
-                multiline
-                fullWidth
-                minRows={5}
-                value={output}
-                onChange={(e) => setOutput(e.target.value)}
-                inputProps={{
-                  style: { color: "white" },
-                  spellCheck: false,
-                }}
-                sx={{ backgroundColor: "#29363f", borderRadius: 3 }}
-              />
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid container item justifyContent="center">
-          <Grid item>
-            <Button
-              size="large"
-              variant="contained"
-              color="secondary"
-              onClick={handleConvert}
-              sx={{ backgroundColor: "#6a329f" }}
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <Grid container item>
+        <MainGrid>
+          <Grid container item mb={3}>
+            <Grid
+              container
+              item
+              direction="column"
+              xs={6}
+              spacing={2}
+              paddingRight={1}
             >
-              Convert
-            </Button>
+              <Grid container item justifyContent="center">
+                <Grid item>
+                  <Typography variant="h4" fontWeight={700} color="white">
+                    Input
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <TextField
+                  multiline
+                  fullWidth
+                  minRows={5}
+                  value={input}
+                  color="primary"
+                  onChange={(e) => setInput(e.target.value)}
+                  inputProps={{
+                    style: { color: "white" },
+                    spellCheck: false,
+                  }}
+                  sx={{ backgroundColor: "#29363f", borderRadius: 3 }}
+                />
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              item
+              direction="column"
+              xs={6}
+              spacing={2}
+              paddingLeft={1}
+            >
+              <Grid container item justifyContent="center">
+                <Grid item>
+                  <Typography variant="h4" fontWeight={700} color="white">
+                    Output
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <TextField
+                  multiline
+                  fullWidth
+                  minRows={5}
+                  value={output}
+                  onChange={(e) => setOutput(e.target.value)}
+                  inputProps={{
+                    style: { color: "white" },
+                    spellCheck: false,
+                  }}
+                  sx={{ backgroundColor: "#29363f", borderRadius: 3 }}
+                />
+              </Grid>
+            </Grid>
           </Grid>
-        </Grid>
-      </MainGrid>
-    </Grid>
+          <Grid container item justifyContent="center">
+            <Grid item>
+              <Button
+                size="large"
+                variant="contained"
+                color="secondary"
+                onClick={handleConvert}
+                sx={{ backgroundColor: "#6a329f" }}
+              >
+                Convert
+              </Button>
+            </Grid>
+          </Grid>
+        </MainGrid>
+      </Grid>
+    </>
   );
 };
 
