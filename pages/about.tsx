5741f441c050a1a5cc5c6e281343d140e94c68d6
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2022-10-09 22:06:41 +0700

    Add dummy about page

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+import MainGrid from '../components/MainGrid'
+
+const About: NextPage = () => {
+  return (
+    <MainGrid>
+      
+    </MainGrid>
+  )
+}
+
+export default About
